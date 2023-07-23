ee09e9081cc499640a35c8a9cc87abbc5953fd09
Muneer128 <114104922+Muneer128@users.noreply.github.com>
2023-07-23 18:54:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function draw() {
+
+}
+
+function check_sketch() {
+
+}
+
+random_number = Math.floor(Math.random()*quick_draw_data_set.legnth)
+function update_canvas() {
+background_color(white)
+}
